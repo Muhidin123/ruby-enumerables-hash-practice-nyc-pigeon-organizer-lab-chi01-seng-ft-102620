53a98bf15da863a2d01e5db715486dc16f2e3f19
Muhidin Hukic <muhidinhukic1@gmail.com>
2020-09-22 17:50:42 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,10 +4,8 @@ def nyc_pigeon_organizer(data)
     value.each do |key_inside, names|
       names.each do |key_inside2|
         solution[key_inside2] = key
-        binding.pry
       end
     end
     solution[key_inside2][key] = key_inside.to_s
-    binding.pry
   end
 end
